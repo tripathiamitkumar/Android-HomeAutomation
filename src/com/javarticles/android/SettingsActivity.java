260f1fe320b750b66fb2cf3db88b32baeab35306
tripathiamitkumar <er.amitkumartripathi@gmail.com>
2018-07-26 18:19:41 +0530

    This app is a part of IoT Home Automation project

---
+++ src/com/javarticles/android/SettingsActivity.java
@@ -0,0 +1,19 @@
+package com.javarticles.android;
+
+import com.example.homeautomation.R;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+
+
+public class SettingsActivity extends PreferenceActivity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+
+}
